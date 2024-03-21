b0e24b1bc460f470f22e85e6070e5f2a608419a2
Lyubawa <lyubawa81@ukr.net>
2024-03-21 07:27:23 +0200

    hw-react-6-1

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,20 @@
+import { useSelector } from 'react-redux';
+import Contact from '../Contact/Contact';
+import css from './ContactList.module.css';
+
+export default function ContactList() {
+  const contacts = useSelector(state => state.contacts.items);
+  return (
+    <ul className={css.list}>
+      {contacts.map(contact => (
+        <li className={css.item} key={contact.id}>
+          <Contact
+            name={contact.name}
+            number={contact.number}
+            id={contact.id}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+}
